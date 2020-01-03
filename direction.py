0426cb8ec42c42c9422226eeb4a4e9356984a106
Riwaj Shrestha <sriwaj@insightworkshop.io>
2020-01-03 16:09:23 +0545

    Added double precision

--- direction.py
+++ direction.py
@@ -1,7 +1,7 @@
 import math
 
 pointA = [37.20553540890865,-89.98048343749997]
-pointB = [37.79155875890545,-90.56378879638669]
+pointB = [37.71481966440855,-89.45142307373044]
 
 LatA = math.radians(pointA[0] ) 
 LongA = math.radians(pointA[1])   
@@ -22,21 +22,37 @@ def getDirection(LatA,LongA,LatB,LongB):
     Be =math.degrees(math.atan2(X,Y))
     # print('Bearing Angle : ',Be)
     if Be == 0 :
-        return ("North")
-    elif 0 < Be < 90:
+        return("North")
+    elif 0 < Be < 45:
+        return("NorthNorthEast")
+    elif Be == 45:
         return("NorthEast")
+    elif 45 < Be < 90:
+        return("NorthEastEast")
     elif Be == 90:
         return("East")
-    elif 90 < Be < 180:
-        return("SouthEast")  
+    elif 90 < Be < 135:
+        return("SouthEastEast") 
+    elif Be == 135:
+        return("SouthEast") 
+    elif 135 < Be < 180:
+        return("SouthSouthEast")  
     elif Be == 180:
         return("South")
-    elif -90 > Be > -180:
-        return("SouthWest") 
+    elif -135 > Be > -180:
+        return("SouthSouthWest")
+    elif Be == -135:
+        return("SouthWest")
+    elif -90 > Be > -135:
+        return("SouthWestWest")   
     elif Be == -90:
         return("West")
-    elif 0 > Be > -90:
-        return("NorthWest")
+    elif 0 > Be > -45:
+        return("NorthNorthWest")
+    elif Be == -45:
+        return("NorthWest")  
+    elif -45 > Be > -90:
+        return("NorthWestWest") 
     else:
         return 'Not a valid location' 
 
